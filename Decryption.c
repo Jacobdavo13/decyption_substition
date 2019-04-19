f827da26f7e5864b5907887c781af48adf4ce038
Jacob Davison <c3324607@uon.edu.au>
2019-04-19 13:25:23 +0000

    Hell yes

--- Decryption.c
+++ Decryption.c
@@ -3,146 +3,150 @@
 int main()
 {
   char let[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  char key[] = "CDEFGHIJKLMNOPQRSTUVWXYZAB"; 
-  //the decryption key for this key will be ZABCDEFGHIJKLMNOPQRSTUVWXYZ
-  char message[] = "JGA FWODQ";
+  char key[] = "NWLRBMQHCDAZOKYISXJFEGPUVT"; 
+  // char key[] = "KEIQUTVHPSNCFAMWGDZJXYBROL" key for physicist quote//
+
+  char message[] = "RCR VYE BGBX HBNX FHB FXNQBRV YM RNXFH IZNQEBCJ FHB PCJB? C FHYEQHF KYF. CF'J KYF N JFYXV FHB DBRC PYEZR FBZZ VYE. CF'J N JCFH ZBQBKR. RNXFH IZNQEBCJ PNJ N RNXA ZYXR YM FHB JCFH, JY IYPBXMEZ NKR JY PCJB HB LYEZR EJB FHB MYXLB FY CKMZEBKLB FHB OCRCLHZYXCNKJ FY LXBNFB ZCMB… HB HNR JELH N AKYPZBRQB YM FHB RNXA JCRB FHNF HB LYEZR BGBK ABBI FHB YKBJ HB LNXBR NWYEF MXYO RVCKQ. FHB RNXA JCRB YM FHB MYXLB CJ N INFHPNV FY ONKV NWCZCFCBJ JYOB LYKJCRBX FY WB EKKNFEXNZ. HB WBLNOB JY IYPBXMEZ… FHB YKZV FHCKQ HB PNJ NMXNCR YM PNJ ZYJCKQ HCJ IYPBX, PHCLH BGBKFENZZV, YM LYEXJB, HB RCR. EKMYXFEKNFBZV, HB FNEQHF HCJ NIIXBKFCLB BGBXVFHCKQ HB AKBP, FHBK HCJ NIIXBKFCLB ACZZBR HCO CK HCJ JZBBI. CXYKCL. HB LYEZR JNGB YFHBXJ MXYO RBNFH, WEF KYF HCOJBZM.";
   int index;
   printf("This was the message %s\n", message);
-  for(index = 0; message[index] != 0; index++)
+  for(index = 0; message[index] != 0; index++) //i need to fix the issue where it declares as one then the if happens again
   {
-      if(message[index] == key[0])
-      {
-          message[index] = let[0];
-          
-      }
-      if(message[index] == key[1])
-      {
-          message[index] = let[1];
-          
-      }
-   if(message[index] == key[2])
-      {
-          message[index] = let[2];
-          
-      }
-      if(message[index] == key[3])
-      {
-          message[index] = let[3];
-          
-      }
-        if(message[index] == key[4])
-      {
-          message[index] = let[4];
-          
-      }
-      if(message[index] == key[5])
-      {
-          message[index] = let[5];
-          
-      }
-   if(message[index] == key[6])
-      {
-          message[index] = let[6];
-          
-      }
-      if(message[index] == key[7])
-      {
-          message[index] = let[7];
-          
-      }
-  if(message[index] == key[8])
-      {
-          message[index] = let[8];
-          
-      }
-      if(message[index] == key[9])
-      {
-          message[index] = let[9];
-          
-      }
-   if(message[index] == key[10])
-      {
-          message[index] = let[10];
-          
-      }
-      if(message[index] == key[11])
-      {
-          message[index] = let[11];
-          
-      }
-        if(message[index] == key[12])
-      {
-          message[index] = let[12];
-          
-      }
-      if(message[index] == key[13])
-      {
-          message[index] = let[13];
-          
-      }
-   if(message[index] == key[14])
-      {
-          message[index] = let[14];
-         
-      }
-      if(message[index] == key[15])
-      {
-          message[index] = let[15];
-          
-      }
-        if(message[index] == key[16])
-      {
-          message[index] = let[16];
-          
-      }
-      if(message[index] == key[17])
-      {
-          message[index] = let[17];
-          
-      }
-   if(message[index] == key[18])
-      {
-          message[index] = let[18];
-          
-      }
-      if(message[index] == key[19])
-      {
-          message[index] = let[19];
-          
-      }
-        if(message[index] == key[20])
-      {
-          message[index] = let[20];
-        
-      }
-      if(message[index] == key[21])
-      {
-          message[index] = let[21];
-          
-      }
-   if(message[index] == key[22])
-      {
-          message[index] = let[22];
-          
-      }
-      if(message[index] == key[23])
-      {
-          message[index] = let[23];
-          
-      }
-        if(message[index] == key[24])
-      {
-          message[index] = let[24];
-          
-      }
-      if(message[index] == key[25])
-      {
-          message[index] = let[25];
-          
-      }
-  
+ if(message[index]==key[0])
+  {
+      message[index] = let[0];
+      goto LOOPEND;
   }
-  printf("This is the message:  %s\n", message);
+  if(message[index]==key[1])
+  {
+      message[index] = let[1];
+      goto LOOPEND;
+  }
+  if(message[index]==key[2])
+  {
+      message[index] = let[2];
+      goto LOOPEND;
+  }
+  if(message[index]==key[3])
+  {
+      message[index] = let[3];
+      goto LOOPEND;
+  }
+  if(message[index]==key[4])
+  {
+      message[index] = let[4];
+      goto LOOPEND;
+  }
+  if(message[index]==key[5])
+  {
+      message[index] = let[5];
+      goto LOOPEND;
+  }
+  if(message[index]==key[6])
+  {
+      message[index] = let[6];
+      goto LOOPEND;
+  }
+  if(message[index]==key[7])
+  {
+      message[index] = let[7];
+      goto LOOPEND;
+  }
+   if(message[index]==key[8])
+  {
+      message[index] = let[8];
+      goto LOOPEND;
+  }
+  if(message[index]==key[9])
+  {
+      message[index] = let[9];
+      goto LOOPEND;
+  }
+  if(message[index]==key[10])
+  {
+      message[index] = let[10];
+      goto LOOPEND;
+  }
+  if(message[index]==key[11])
+  {
+      message[index] = let[11];
+      goto LOOPEND;
+  }
+  if(message[index]==key[12])
+  {
+      message[index] = let[12];
+      goto LOOPEND;
+  }
+  if(message[index]==key[13])
+  {
+      message[index] = let[13];
+      goto LOOPEND;
+  }
+  if(message[index]==key[14])
+  {
+      message[index] = let[14];
+      goto LOOPEND;
+  }
+  if(message[index]==key[15])
+  {
+      message[index] = let[15];
+      goto LOOPEND;
+  }
+   if(message[index]==key[16])
+  {
+      message[index] = let[16];
+      goto LOOPEND;
+  }
+  if(message[index]==key[17])
+  {
+      message[index] = let[17];
+      goto LOOPEND;
+  }
+  if(message[index]==key[18])
+  {
+      message[index] = let[18];
+      goto LOOPEND;
+  }
+  if(message[index]==key[19])
+  {
+      message[index] = let[19];
+      goto LOOPEND;
+  }
+   if(message[index]==key[20])
+  {
+      message[index] = let[20];
+      goto LOOPEND;
+  }
+  if(message[index]==key[21])
+  {
+      message[index] = let[21];
+      goto LOOPEND;
+  }
+  if(message[index]==key[22])
+  {
+      message[index] = let[22];
+      goto LOOPEND;
+  }
+  if(message[index]==key[23])
+  {
+      message[index] = let[23];
+      goto LOOPEND;
+  }
+   if(message[index]==key[24])
+  {
+      message[index] = let[24];
+      goto LOOPEND;
+  }
+  if(message[index]==key[25])
+  {
+      message[index] = let[25];
+      goto LOOPEND;
+  }
+LOOPEND:{
+    //DO NOTHING
+}
+  //do nothing
+  }
+  printf("The Decrypted Message is:  %s\n", message);
   
   }
   
